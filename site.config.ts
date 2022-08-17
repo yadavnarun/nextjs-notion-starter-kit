cd71ef5be70d3eeb6ee71a45cd819feea12f30a7
Narun Yadav <n.y.narun@gmail.com>
2022-08-18 01:23:55 +0530

    feat: added commitizen

--- site.config.ts
+++ site.config.ts
@@ -1,39 +1,27 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
   rootNotionPageId: 'c58e4f1fe41d47bd9729f950634082b0',
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
-  // basic site info (required)
   name: 'JsBs',
   domain: 'colorful-centipede.notion.site',
   author: 'Narun Yadav',
 
-  // open graph metadata (optional)
   description: '',
 
-  // social usernames (optional)
   twitter: 'NarunYadav',
   github: 'yadavnarun',
   linkedin: 'narunyadav',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
   isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
